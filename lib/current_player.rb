3737ce131dcd22f465d90dac6c3fe4220702feb8
Paris Dolfman <parisdolfman@gmail.com>
2017-12-02 19:10:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,7 @@
 def turn_count(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   counter = 0
+  board.each do |space|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}"
 end
